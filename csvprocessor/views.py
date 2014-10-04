77f388f2c27d501ec11e06c179c5ddb0ee4d4b57
alex <atey188@gmail.com>
2014-10-04 18:48:16 -0400

    added budget and categories

--- csvprocessor/views.py
+++ csvprocessor/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from budgetapp.models import Purchases, Paychecks, Category
+from budgetapp.models import Purchases, Paychecks, SubCategory, SuperCategory
 from budgetapp.mcc import MintCloudClient
+from django.db import IntegrityError
 import csv
 
 import datetime
@@ -12,28 +13,25 @@ import datetime
 from csvprocessor.forms import UploadFileForm
 
 CATEGORY_MAP = {
-    "coffee shops" : "coffee",
     "electronics & software"  : "electronics",
     "gas & fuel" : "gas",
     "mortgage & rent" : "rent",
     "shopping" : "miscellaneous",
-    "dentist" : "doctor",
     "pharmacy" : "personal care",
-    "service & parts" : "maintenance",
-    "fast food" : "restaurants",
-    "miscellaneous fee" : "misc fee",
-    "finance charges" : "finance charge",
     "gym membership" : "gym",
     "home insurance bill" : "home insurance",
-    "atm fee": "misc fee"
+    "atm fee": "misc fee",
+    "cash & atm": "miscellaneous fee",
 }
 
 SKIP_MAP = ["credit card payment", 
             "exclude from mint", 
+            "hide from budgets & trends", 
             "interest income", 
             "loan payment", 
             "transfer", 
-            "cash & atm"]
+            "cash & atm",
+            "home down payment"]
 
 def uploadcsv(request):
     # Handle file upload
@@ -79,7 +77,6 @@ def handle_uploaded_file(f):
 
     for transaction in dataReader:
         if transaction[0] != 'Date': # Ignore the header row, import everything else
-
             date = transaction[0]
             new_date = datetime.datetime.strptime(date, "%m-%d%-Y").date()
             date = new_date.strftime("%Y-%m-%d")
@@ -94,21 +91,54 @@ def handle_uploaded_file(f):
 
             save_transaction(new_transaction)
 
-def fetch_transactions(request):
+def start_connection():
     u = "atey188@gmail.com"
     p = "A131681012a"
     c = MintCloudClient()
     c.login(u, p)
+    return c
+
+
+def fetch_budget():
+    c = start_connection()
+    categories = c.getBudget()
+    categories = categories["set"][0]["data"]
+    category_dict = {}
+    for category in categories:
+       category_id = category["id"]
+       category_value = category["value"]
+       category_dict[category_id] = category_value
+       for child_cat in category["children"]:
+          category_dict[child_cat["id"]] = child_cat["value"]
+    return category_dict       
+     
+
+
+def fetch_categories():
+    c = start_connection()
+    categories = c.getCategories()
+    categories = categories["set"][0]["data"]
+    category_dict = {}
+    for category in categories:
+       category_id = category["id"]
+       category_value = category["value"]
+       category_dict[category_id] = category_value
+       for child_cat in category["children"]:
+          category_dict[child_cat["id"]] = child_cat["value"]
+    return category_dict       
+     
+
+def fetch_transactions(request):
+    c = start_connection()
 
     Purchases.objects.all().delete()
     Paychecks.objects.all().delete()
 
     transaction_list = []
     skip_transaction_list = []
-
+    excluded_pids = set()
     transactions = c.allTransactions()
     for transaction in transactions:
-
         #description_mmerchant = transaction["mmerchant"]
         #description-merchant = transaction["merchant"]
         #description-ruleMerchant = transaction["ruleMerchant"]
@@ -118,6 +148,7 @@ def fetch_transactions(request):
         #ruleCategory = transaction["ruleCategory"]
 
         date = transaction["date"]
+        mint_id = transaction["id"]
 
         if len(date.split("/")) <= 1:
             date = date.split(' ')
@@ -144,21 +175,36 @@ def fetch_transactions(request):
         amount = amount[1:]
         amount = amount.replace(",", "")
 
+        if transaction['isChild']:
+           excluded_pid = transaction['pid'] 
+           excluded_pids.add(excluded_pid)
+         
+        if mint_id in excluded_pids or transaction['isPending']:
+           #category = "Exclude From Mint"
+           continue
+
         #there's afew weird split transaction that I can't get out of mint, exclude it manually
-        if description == 'Bamboo' and amount == "193.76":
-            category = 'Exclude From Mint'
-        if description == 'Good Gas' and amount == "218.91":
-            category = 'Exclude From Mint'
+        #if description == 'Bamboo' and amount == "193.76":
+        #    category = 'Exclude From Mint'
+        #if description == 'Good Gas' and amount == "218.91":
+        #    category = 'Exclude From Mint'
+        #if description == "Hilton" and mint_id == 108092011:
+        #    category = 'Exclude From Mint'
+        #if description == "Target - jacket, blouse, lotion, cleaning supplies" and mint_id == 294993556:
+        #    category = 'Exclude From Mint'
+        #if mint_id == 299890567:
+        #    category = 'Exclude From Mint'
 
         #skip pending transaction
-        if transaction['isPending']:
-            category = 'Exclude From Mint'
+        #if transaction['isPending']:
+        #    category = 'Exclude From Mint'
 
         transaction_type = "debit" if transaction["isDebit"] else "credit"
         account = transaction["account"]
         notes = transaction["note"]
 
-        new_transaction = {'date': date, 'description': description, 'amount': amount, 'transaction_type': transaction_type, 'category':category, 'notes': notes}
+
+        new_transaction = {'mint_id': mint_id, 'date': date, 'description': description, 'amount': amount, 'transaction_type': transaction_type, 'category':category, 'notes': notes}
         if save_transaction(new_transaction):
             transaction_list.append(new_transaction)
         else:
@@ -179,6 +225,7 @@ def save_transaction(transaction):
     transaction_type = transaction["transaction_type"]
     category = transaction["category"]
     notes = transaction["notes"]
+    mint_id = transaction["mint_id"]
 
     #date needs to be passed in with the proper format (from mm/dd/yyyy to yyyy-mm-dd)
     #date = date.split("/")
@@ -223,8 +270,11 @@ def save_transaction(transaction):
         paycheck.net = amount * -1
         paycheck.gross = amount * -1
         paycheck.date = date
-
+        paycheck.mint_id = mint_id
+        if Paychecks.objects.filter(mint_id=mint_id):
+            import pdb; pdb.set_trace()        
         paycheck.save()
+
     #else if category == skip
     elif category == "skip":
         return False
@@ -233,17 +283,20 @@ def save_transaction(transaction):
         purchase = Purchases();
 
         try:
-            category_object = Category.objects.get(subCategory=category)
+            category_object = SubCategory.objects.get(subCategory=category)
         except Category.DoesNotExist:
-            #import pdb; pdb.set_trace()
-            category_object = Category.objects.get(subCategory="unknown")
+            import pdb; pdb.set_trace()
+            category_object = SubCategory.objects.get(subCategory="unknown")
 
 
         purchase.date = date
         purchase.item_desc = description
         purchase.cost = amount
         purchase.category = category_object
+        purchase.mint_id = mint_id
 
+        if Purchases.objects.filter(mint_id=mint_id):
+            import pdb; pdb.set_trace()        
         purchase.save()
 
     return True
